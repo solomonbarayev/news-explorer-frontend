d401a2329dd32ba626ea54acbc5d4a18be65c82e
solomonbarayev <sbarayev2@gmail.com>
2022-11-04 12:08:11 +0200

    add NotFound component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import SearchResults from '../SearchResults/SearchResults';
 import AboutAuthor from '../AboutAuthor/AboutAuthor';
-import Preloader from '../Preloader/Preloader';
+// import Preloader from '../Preloader/Preloader';
+import NotFound from '../NotFound/NotFound';
 
 const Main = () => {
   return (
     <>
       <main className="main">
         {/* <Preloader /> */}
+        {/* <NotFound /> */}
         <SearchResults />
         <AboutAuthor />
       </main>
