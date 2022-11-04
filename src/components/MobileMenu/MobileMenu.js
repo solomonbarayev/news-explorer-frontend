8f5c6769bd687758f1e1afaf25bd0090472697b6
solomonbarayev <sbarayev2@gmail.com>
2022-11-04 11:30:46 +0200

    get rid of uneccessary wrapping div overlay in mobile menu

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -25,57 +25,55 @@ const MobileMenu = () => {
       className={`${
         popupsState.mobile ? 'mobile-menu mobile-menu_opened' : 'mobile-menu'
       }`}>
-      <div className="mobile-menu__overlay">
-        <div className="mobile-menu__container">
-          <div className="mobile-menu__top-bar">
-            <div className="mobile-menu__logo-container">
-              <img src={logo} alt="logo" className="mobile-menu__logo" />
-            </div>
-            <button
-              type="button"
-              className="mobile-menu__close-button"
-              aria-label="close button"
-              onClick={closeAllPopups}></button>
+      <div className="mobile-menu__container">
+        <div className="mobile-menu__top-bar">
+          <div className="mobile-menu__logo-container">
+            <img src={logo} alt="logo" className="mobile-menu__logo" />
           </div>
-          <nav className="mobile-menu__nav">
-            <ul className="mobile-menu__nav-list">
+          <button
+            type="button"
+            className="mobile-menu__close-button"
+            aria-label="close button"
+            onClick={closeAllPopups}></button>
+        </div>
+        <nav className="mobile-menu__nav">
+          <ul className="mobile-menu__nav-list">
+            <li className="mobile-menu__list-item">
+              <NavLink
+                exact={true}
+                to="/"
+                className="mobile-menu__link"
+                activeClassName="mobile-menu__link_active">
+                Home
+              </NavLink>
+            </li>
+            {loggedIn && (
               <li className="mobile-menu__list-item">
                 <NavLink
-                  exact={true}
-                  to="/"
+                  to="/saved-news"
                   className="mobile-menu__link"
                   activeClassName="mobile-menu__link_active">
-                  Home
+                  Saved articles
                 </NavLink>
               </li>
-              {loggedIn && (
-                <li className="mobile-menu__list-item">
-                  <NavLink
-                    to="/saved-news"
-                    className="mobile-menu__link"
-                    activeClassName="mobile-menu__link_active">
-                    Saved articles
-                  </NavLink>
-                </li>
-              )}
-            </ul>
+            )}
+          </ul>
 
-            <button
-              className="mobile-menu__button"
-              onClick={handleAuthButtonClick}>
-              <span className="mobile-menu__button-text">
-                {loggedIn ? user.firstName : 'Sign in'}
-              </span>
-              {loggedIn && (
-                <img
-                  className="mobile-menu__icon"
-                  src={logOutIcon}
-                  alt="logout"
-                />
-              )}
-            </button>
-          </nav>
-        </div>
+          <button
+            className="mobile-menu__button"
+            onClick={handleAuthButtonClick}>
+            <span className="mobile-menu__button-text">
+              {loggedIn ? user.firstName : 'Sign in'}
+            </span>
+            {loggedIn && (
+              <img
+                className="mobile-menu__icon"
+                src={logOutIcon}
+                alt="logout"
+              />
+            )}
+          </button>
+        </nav>
       </div>
     </div>
   );
