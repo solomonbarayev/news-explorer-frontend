cd51b4f1486011f9884deb9df4b22eb903f54cd5
solomonbarayev <sbarayev2@gmail.com>
2022-11-29 20:15:32 +0200

    change popup overlay click to not have to access dom directly

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import './Popup.css';
 import { usePopup } from '../../contexts/PopupsContext';
 
@@ -11,36 +11,33 @@ type PopupProps = {
 const Popup = ({ isOpen, name, children }: PopupProps) => {
   const popupsContext = usePopup();
 
+  const stopOverlayClickPropogation = (evt: React.MouseEvent) => {
+    evt.stopPropagation();
+  };
+
+  const handleEscClose = useCallback(
+    (evt) => {
+      if (evt.key === 'Escape' && isOpen) {
+        popupsContext.closeAllPopups();
+      }
+    },
+    [isOpen, popupsContext.closeAllPopups]
+  );
+
   useEffect(() => {
-    if (!isOpen) return;
-
-    function handleEscClose(evt) {
-      if (evt.key === 'Escape') {
-        popupsContext.closeAllPopups();
-      }
-    }
-
-    function handleOverlayClickClose(evt) {
-      if (evt.target.classList.contains('popup_opened')) {
-        popupsContext.closeAllPopups();
-      }
-    }
-
-    // change this to not touch the dom directly
-    // from here
     document.addEventListener('keydown', handleEscClose);
-    document.addEventListener('click', handleOverlayClickClose);
-
     return () => {
       document.removeEventListener('keydown', handleEscClose);
-      document.removeEventListener('click', handleOverlayClickClose);
     };
-    // to here
-  }, [isOpen, popupsContext.closeAllPopups]);
+  }, [handleEscClose]);
 
   return (
-    <div className={`popup ${isOpen ? 'popup_opened' : ''} popup_type_${name}`}>
-      <div className={`popup__container popup__container_type_${name}`}>
+    <div
+      className={`popup ${isOpen ? 'popup_opened' : ''} popup_type_${name}`}
+      onClick={popupsContext.closeAllPopups}>
+      <div
+        className={`popup__container popup__container_type_${name}`}
+        onClick={stopOverlayClickPropogation}>
         {children}
         <button
           className={`popup__close-button ${`popup__close-button_type_${name}`}`}
