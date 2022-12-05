829a3cf1bb05dbc8607e8bb5a9696e67c7a4fcd4
solomonbarayev <sbarayev2@gmail.com>
2022-12-05 22:22:11 +0200

    open signin popup if saved-news page attempted without login

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../store';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState, AppDispatch } from '../../store';
+import { openPopup } from '../../features/popups/popupsSlice';
 
 type ProtectedRouteProps = {
   children: React.ReactNode;
@@ -12,11 +13,16 @@ type ProtectedRouteProps = {
 const ProtectedRoute = ({ children, path }: ProtectedRouteProps) => {
   const { loggedIn } = useSelector((state: RootState) => state.user);
 
-  return loggedIn ? (
-    <Route path={path}>{children}</Route>
-  ) : (
-    <Redirect to="/signin" />
-  );
+  //if attempting to open saved-news and not logged in, open signin popup
+  useEffect(() => {
+    if (path === '/saved-news' && !loggedIn) {
+      dispatch(openPopup('signin'));
+    }
+  }, [path, loggedIn]);
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  return loggedIn ? <Route path={path}>{children}</Route> : <Redirect to="/" />;
 };
 
 export default ProtectedRoute;
